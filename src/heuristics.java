e99ae8a8576a0fc04e8d842085538afa5c7b2ee3
Duc Pham <dmpham@wpi.edu>
2015-09-09 21:03:57 -0400

    Add heuristic functions

---
+++ src/heuristics.java
@@ -0,0 +1,40 @@
+import java.lang.Math.*;
+
+public class heuristics {
+	public int heuristic1(int currentlocX, int currentlocY, int goallocX, int goallocY ){
+		return 0;
+	}
+	public int heuristic2(int currentlocX, int currentlocY, int goallocX, int goallocY ){
+		int vertical = Math.abs(currentlocX - goallocX);
+		int horizontal = Math.abs(currentlocY - goallocY);
+		int result = Math.min(vertical, horizontal);
+		return result;
+	}
+	public int heuristic3(int currentlocX, int currentlocY, int goallocX, int goallocY ){
+		int vertical = Math.abs(currentlocX - goallocX);
+		int horizontal = Math.abs(currentlocY - goallocY);
+		int result = Math.max(vertical, horizontal);
+		return result;
+	}
+	public int heuristic4(int currentlocX, int currentlocY, int goallocX, int goallocY ){
+		int vertical = Math.abs(currentlocX - goallocX);
+		int horizontal = Math.abs(currentlocY - goallocY);
+		int result = vertical + horizontal;
+		return result;
+	}
+	public int heuristic5(int currentlocX, int currentlocY, int goallocX, int goallocY ){
+		int vertical = Math.abs(currentlocX - goallocX);
+		int horizontal = Math.abs(currentlocY - goallocY);
+		int d = 1-2;
+		int result = (vertical + horizontal) + d*Math.min(vertical, horizontal);
+		return result;
+	}
+	public int heuristic6(int currentlocX, int currentlocY, int goallocX, int goallocY ){
+		int vertical = Math.abs(currentlocX - goallocX);
+		int horizontal = Math.abs(currentlocY - goallocY);
+		int d = 1-2;
+		int result = (vertical + horizontal) + d*Math.min(vertical, horizontal);
+		return 3*result;
+	}
+	
+}
